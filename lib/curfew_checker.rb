d045c524545101668cee07a340c444ae756c8159
NOORABDELHALIM <nur.abdelhalim@rebels.ridgenet.org>
2018-08-08 14:15:51 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -39,6 +39,6 @@ def deluxe_curfew_checker(time , curfew = 11)
 end
 deluxe_curfew_checker(3)
 
-# def platinum_curfew_checker(current_time, curfew_time)
-#   # code goes here
-# end
+def platinum_curfew_checker(current_time, curfew_time)
+  # code goes here
+end
